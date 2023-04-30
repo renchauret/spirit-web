23166b9e2d22817a63d33997174e6581a45f16b9
Ren Chauret <renaudchauret@gmail.com>
2023-04-29 22:55:49 -0400

    router and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './App.css'
 import { SignIn } from './components/SignIn'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export const App = () => {
     return (
         <div className="App">
-            <SignIn />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="" element={<div>yo</div>} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
