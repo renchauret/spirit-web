c400c88d0ce77e2e7ef3c1d990ec21874cd0ecd0
Ren Chauret <renaudchauret@gmail.com>
2023-05-05 09:53:10 -0400

    split out gallery

--- src/components/Drinks.tsx
+++ src/components/Drinks.tsx
@@ -1,38 +1,19 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useGetDrinks } from '../hooks/Drinks'
 import { GalleryImage } from './GalleryImage'
-import { Waypoint } from 'react-waypoint'
+import { Gallery } from './Gallery'
 
 export const Drinks = () => {
     const [page, setPage] = useState(1)
     const { data: drinks } = useGetDrinks(page)
 
-    const handleScroll = useCallback(() => {
-        setPage(page + 1)
-    }, [page, setPage])
-
-    const images = useMemo(() => {
-            const imageList = (drinks && drinks.map(drink =>
+    const images = useMemo(() =>
+            (drinks && drinks.map(drink =>
                 <GalleryImage id={drink.guid} imageUrl={drink.imagePath ?? 'assets/drink.png'} linkUrl="/"
                               invert={Boolean(!drink.imagePath)} name={drink.name} />
-            )) || []
-            if (imageList.length > 1) imageList.push(<Waypoint onEnter={handleScroll} />)
-            return imageList
-        },
-        [drinks, handleScroll]
+            )) || [],
+        [drinks]
     )
 
-    if (!images || images.length === 0) return <></>
-
-    return (
-        <div
-            className="mx-auto my-4 text-center content-center md:w-4/5"
-            id="gallery"
-            onScroll={handleScroll}
-        >
-            <div className="grid my-0 mx-auto w-full list-none grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-                {images}
-            </div>
-        </div>
-    )
+    return <Gallery images={images} page={page} setPage={setPage} />
 }
---
+++ src/components/Gallery.tsx
@@ -0,0 +1,35 @@
+import React, { useCallback, useMemo } from 'react'
+import { GalleryImage } from './GalleryImage'
+import { Waypoint } from 'react-waypoint'
+
+interface GalleryProps {
+    images: JSX.Element[],
+    page: number,
+    setPage: React.Dispatch<React.SetStateAction<number>>
+}
+
+export const Gallery = ({ images, page, setPage }: GalleryProps) => {
+    const handleScroll = useCallback(() => {
+        setPage(page + 1)
+    }, [page, setPage])
+
+    const finalImages = useMemo(() => {
+            return (images.length > 1) ? [...images, <Waypoint onEnter={handleScroll} />] : images
+        },
+        [images, handleScroll]
+    )
+
+    if (!finalImages || finalImages.length === 0) return <></>
+
+    return (
+        <div
+            className="mx-auto my-4 text-center content-center md:w-4/5"
+            id="gallery"
+            onScroll={handleScroll}
+        >
+            <div className="grid my-0 mx-auto w-full list-none grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+                {finalImages}
+            </div>
+        </div>
+    )
+}
