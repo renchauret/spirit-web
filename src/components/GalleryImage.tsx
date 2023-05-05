221e96edab20be7c74e34392e5c24f163409f390
Ren Chauret <renaudchauret@gmail.com>
2023-05-04 20:05:51 -0400

    bg color

--- src/components/GalleryImage.tsx
+++ src/components/GalleryImage.tsx
@@ -11,11 +11,12 @@ interface GalleryImageProps {
 export const GalleryImage = ({ id, imageUrl, linkUrl, invert = false, name = null }: GalleryImageProps) => {
     return (
         <span>
-            {/*className="w-full md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6">*/}
             <a href={linkUrl}>
                 {name &&
                     <div
-                        className="pointer-events-none text-left ml-1 mt-1 z-20 absolute cursor-pointer py-1 px-2 rounded bg-gray-800">
+                        className="pointer-events-none text-left ml-1 mt-1 z-20 absolute cursor-pointer py-1 px-2 rounded"
+                        style={{ backgroundColor: 'var(--bg-color)' }}
+                    >
                         {name}
                     </div>
                 }
